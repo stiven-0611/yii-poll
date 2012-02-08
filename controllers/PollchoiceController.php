9cdcb9c10207de2c9f1e4dfa6feedf57fdee5829
matt <matt@the-endless.local>
2012-02-08 17:28:59 -0500

    Moved controllers back to extending from Controller

--- controllers/PollchoiceController.php
+++ controllers/PollchoiceController.php
@@ -1,23 +1,7 @@
 <?php
 
-class PollchoiceController extends CController
+class PollchoiceController extends Controller
 {
-  /**
-   * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
-   * using two-column layout. See 'protected/views/layouts/column2.php'.
-   */
-  public $layout='//layouts/column2';
-  /**
-   * @var array context menu items. This property will be assigned to {@link CMenu::items}.
-   */
-  public $menu=array();
-  /**
-   * @var array the breadcrumbs of the current page. The value of this property will
-   * be assigned to {@link CBreadcrumbs::links}. Please refer to {@link CBreadcrumbs::links}
-   * for more details on how to specify this property.
-   */
-  public $breadcrumbs=array();
-
   /**
    * @return array action filters
    */
